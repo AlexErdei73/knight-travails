ae918de17e8a1f9e65ee12341434b83a6770cdb4
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-04 11:15:13 +0100

    Add knight.ts module to project

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import chessBoard from "./chessBoard";
+import knight from "./knight";
 
-const knight = document.querySelector(".knight")!;
-const ANIM_STEP_DURATION = 500;
-
-const positions = [
+const POSITIONS: [x: number, y: number][] = [
   [5, 5],
   [6, 3],
   [7, 1],
@@ -11,32 +9,6 @@ const positions = [
   [3, 3],
 ];
 
-function putKnightTo(x: number, y: number) {
-  let style = `top: ${87.5 - 12.5 * y}%; left: ${12.5 * x}%`;
-  knight.setAttribute("style", style);
-  knight.setAttribute("data-y", y.toString());
-}
-
-function moveKnightTo(x: number, y: number) {
-  const oldY = +knight.getAttribute("data-y")!;
-  setTimeout(() => putKnightTo(x, oldY), 0);
-  setTimeout(() => putKnightTo(x, y), ANIM_STEP_DURATION);
-}
-
-function animateKnight() {
-  let i = 0;
-  setInterval(() => {
-    if (i === positions.length) {
-      i = 0;
-      knight.classList.remove("animate");
-    }
-    if (i === 1) knight.classList.add("animate");
-    const [x, y] = positions[i];
-    if (i === 0) putKnightTo(x, y);
-    else moveKnightTo(x, y);
-    i++;
-  }, 3 * ANIM_STEP_DURATION);
-}
-
 chessBoard.create();
-animateKnight();
+knight.positions = POSITIONS;
+knight.animate();
---
+++ src/knight.ts
@@ -0,0 +1,43 @@
+class Knight {
+  private knight: HTMLElement;
+  private _positions: [x: number, y: number][] = [];
+  private ANIM_STEP_DURATION = 500;
+  constructor(knight: HTMLElement) {
+    this.knight = knight;
+  }
+
+  private putTo(position: [x: number, y: number]) {
+    const [x, y] = position;
+    let style = `top: ${87.5 - 12.5 * y}%; left: ${12.5 * x}%`;
+    this.knight.setAttribute("style", style);
+    this.knight.setAttribute("data-pos", JSON.stringify(position));
+  }
+
+  private moveTo(position: [x: number, y: number]) {
+    const x = position[0];
+    const oldY = JSON.parse(this.knight.getAttribute("data-pos")!)[1];
+    setTimeout(() => this.putTo([x, oldY]), 0);
+    setTimeout(() => this.putTo(position), this.ANIM_STEP_DURATION);
+  }
+
+  animate() {
+    let i = 0;
+    setInterval(() => {
+      if (i === this._positions.length) {
+        i = 0;
+        this.knight.classList.remove("animate");
+      }
+      if (i === 1) this.knight.classList.add("animate");
+      const nextPosition = this._positions[i];
+      if (i === 0) this.putTo(nextPosition);
+      else this.moveTo(nextPosition);
+      i++;
+    }, 3 * this.ANIM_STEP_DURATION);
+  }
+
+  set positions(positions: [x: number, y: number][]) {
+    this._positions = positions;
+  }
+}
+
+export default new Knight(document.querySelector(".knight")!);

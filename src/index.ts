07510ff144ebac6d6fddbbb21fe4aea23e3b01b4
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-04 23:50:00 +0100

    Add button to swap chessBoard with form

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,33 @@ const POSITIONS: [x: number, y: number][] = [
 chessBoard.create();
 knight.positions = POSITIONS;
 knight.animate();
+
+const formContainer = document.querySelector(".form-container")!;
+const buttonSwap = document.querySelector("#btn-swap")!;
+
+function handleSwapFormWithChessBoard() {
+  // The main point is toggling the visible element
+  // the rest is just the animation
+  const ANIMATION_DURATION = 500;
+  const DELAY = 20;
+  if (buttonSwap.textContent === "Form") {
+    buttonSwap.textContent = "Chess";
+    chessBoard.boardContainer.classList.add("spin-out");
+  } else {
+    buttonSwap.textContent = "Form";
+    formContainer.classList.add("spin-out");
+  }
+  setTimeout(() => {
+    formContainer.classList.toggle("invisible");
+    chessBoard.boardContainer.classList.toggle("invisible");
+  }, ANIMATION_DURATION);
+  setTimeout(() => {
+    if (formContainer.classList.contains("invisible")) {
+      chessBoard.boardContainer.classList.remove("spin-out");
+    } else {
+      formContainer.classList.remove("spin-out");
+    }
+  }, ANIMATION_DURATION + DELAY); //The previous step takes a few ms, hence the delay
+}
+
+buttonSwap.addEventListener("click", handleSwapFormWithChessBoard);

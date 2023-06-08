b56d25acfdb7cd6cbd0e64509f88cd6a619158c0
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-08 10:28:17 +0100

    Add labels to rows in chessBoard

--- src/chessBoard.ts
+++ src/chessBoard.ts
@@ -8,14 +8,25 @@ class ChessBoard {
 		for (let row = 0; row < 8; row++) {
 			for (let col = 0; col < 8; col++) {
 				if (row === 0) {
+					const letters = ["a", "b", "c", "d", "e", "f", "g", "h"];
 					const colLabelDiv = document.createElement("div");
-					colLabelDiv.textContent = col.toString();
+					colLabelDiv.textContent = letters[col];
 					colLabelDiv.classList.add("col-label");
 					colLabelDiv.setAttribute(
 						"style",
 						`left: calc(${col * 12.5 + 6.25}% - .25rem`
 					);
 					this.boardContainer.appendChild(colLabelDiv);
+				}
+				if (col === 0) {
+					const rowLabelDiv = document.createElement("div");
+					rowLabelDiv.textContent = (row + 1).toString();
+					rowLabelDiv.classList.add("row-label");
+					rowLabelDiv.setAttribute(
+						"style",
+						`top: calc(${(7 - row) * 12.5 + 6.25}% - .5rem`
+					);
+					this.boardContainer.appendChild(rowLabelDiv);
 				}
 				const boardCell = document.createElement("div");
 				boardCell.classList.add("board-cell");

24b23053ab4b18f8bf7fc5335355349bf98b81e1
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-06-08 10:05:35 +0100

    Add captions to the columns in chessBoard

--- src/chessBoard.ts
+++ src/chessBoard.ts
@@ -1,25 +1,35 @@
 class ChessBoard {
-  boardContainer: HTMLElement;
-  constructor(boardContainer: HTMLElement) {
-    this.boardContainer = boardContainer;
-  }
+	boardContainer: HTMLElement;
+	constructor(boardContainer: HTMLElement) {
+		this.boardContainer = boardContainer;
+	}
 
-  create(): void {
-    for (let row = 0; row < 8; row++) {
-      for (let col = 0; col < 8; col++) {
-        const boardCell = document.createElement("div");
-        boardCell.classList.add("board-cell");
-        let color;
-        if (col % 2 === 0) color = "bright";
-        else color = "dark";
-        if (row % 2 === 1)
-          if (color === "bright") color = "dark";
-          else color = "bright";
-        boardCell.classList.add(color);
-        this.boardContainer.appendChild(boardCell);
-      }
-    }
-  }
+	create(): void {
+		for (let row = 0; row < 8; row++) {
+			for (let col = 0; col < 8; col++) {
+				if (row === 0) {
+					const colLabelDiv = document.createElement("div");
+					colLabelDiv.textContent = col.toString();
+					colLabelDiv.classList.add("col-label");
+					colLabelDiv.setAttribute(
+						"style",
+						`left: calc(${col * 12.5 + 6.25}% - .25rem`
+					);
+					this.boardContainer.appendChild(colLabelDiv);
+				}
+				const boardCell = document.createElement("div");
+				boardCell.classList.add("board-cell");
+				let color;
+				if (col % 2 === 0) color = "bright";
+				else color = "dark";
+				if (row % 2 === 1)
+					if (color === "bright") color = "dark";
+					else color = "bright";
+				boardCell.classList.add(color);
+				this.boardContainer.appendChild(boardCell);
+			}
+		}
+	}
 }
 
 export default new ChessBoard(document.querySelector(".chess-board")!);
